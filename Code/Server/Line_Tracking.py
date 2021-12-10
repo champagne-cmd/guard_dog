f1d45ca53a79e6225107199f13a2db39e2281f04
jdnhix <jdhix98890@yahoo.com>
2021-12-10 11:21:36 -0600

    fix

--- Code/Server/Line_Tracking.py
+++ Code/Server/Line_Tracking.py
@@ -51,7 +51,7 @@ class Line_Tracking:
             logging.debug("detected something %d cm away", distance)
 
 
-            if(self.ultrasonic_sensor.get_distance() <= 10):
+            if(distance <= 10):
                 count += 1
 
             initial_run=False
