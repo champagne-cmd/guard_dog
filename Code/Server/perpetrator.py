a1b8247d67a5928bf4f2830a9050301eb429122f
Chloe Champagne <chloe.a.champagne@vanderbilt.edu>
2021-12-10 10:23:22 -0600

    modified perp file

--- Code/Server/perpetrator.py
+++ Code/Server/perpetrator.py
@@ -5,9 +5,9 @@ import time
 def head_on_attack():
     motor = Motor()
     motor.setMotorModel(700,700,700,700)
-    time.sleep(2)
-    motor.setMotorModel(-800,-800,-800,-800)
-    time.sleep(3)
+    time.sleep(3.5)
+    motor.setMotorModel(-700,-700,-700,-700)
+    time.sleep(4)
     motor.setMotorModel(0,0,0,0)
 
 def side_attack():
